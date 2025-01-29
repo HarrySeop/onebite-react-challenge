ce6a18d128ff92516eb301de2263c0d843c5c4dd
HarrySeop <harryseop@gmail.com>
2025-01-29 22:15:23 +0900

    ✨Feat: 8일차 미션 제출

---
+++ Day8/src/index.js
@@ -0,0 +1,21 @@
+import { getRandomName, getRandomEmail, getRandomBirthday, getRandomAddress } from './random.js';
+
+const name1 = getRandomName();
+const name2 = getRandomName();
+const name3 = getRandomName();
+console.log(name1, name2, name3);
+
+const email1 = getRandomEmail();
+const email2 = getRandomEmail();
+const email3 = getRandomEmail();
+console.log(email1, email2, email3);
+
+const birthday1 = getRandomBirthday();
+const birthday2 = getRandomBirthday();
+const birthday3 = getRandomBirthday();
+console.log(birthday1, birthday2, birthday3);
+
+const address1 = getRandomAddress();
+const address2 = getRandomAddress();
+const address3 = getRandomAddress();
+console.log(address1, address2, address3);
